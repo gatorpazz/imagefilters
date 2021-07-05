e640a0342639d825ead44ac9eaf9fd3e5773157a
Michael Pascuzzi <gatorpazz@gmail.com>
2021-07-05 18:39:59 -0400

    Adding grayscale image

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,30 @@ use pyo3::wrap_pymodule;
 mod submodule;
 use submodule::*;
 
+extern crate image;
+
+use image::GenericImageView;
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn grayscale(img: &str) -> PyResult<bool> {
+
+    let img = image::open(str).unwrap();
+
+    let grey = img.grayscale();
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
+
+    // Write the contents of this image to the Writer in PNG format.
+    grey.save("test.png").unwrap();
+    Ok(true)
 }
 
 #[pymodule]
 fn filters(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(grayscale, m)?)?;
 
     Ok(())
 }
