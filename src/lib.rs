8b69c3636ed5dcc3835d12b2d9816375143c2f8e
Michael Pascuzzi <gatorpazz@gmail.com>
2021-07-20 16:16:50 -0400

    Added brighten function

--- src/lib.rs
+++ src/lib.rs
@@ -12,16 +12,26 @@ fn grayscale(img: &str, filename: &str) -> PyResult<bool> {
     Ok(true)
 }
 
-fn blur(img: &str, filename: &str, sigma: f32) -> PyResult<bool> {
+fn blur(img: &str, filename: &str, sigma: &f32) -> PyResult<bool> {
     let img = image::open(img).unwrap();
-    let grey = img.blur(sigma);
+    let grey = img.blur(*sigma);
     grey.save(filename).unwrap();
     Ok(true)
 }
+
+fn brighten(img: &str, filename: &str, value: &i32) -> PyResult<bool> {
+    let img = image::open(img).unwrap();
+    let grey = img.brighen(value);
+    grey.save(filename).unwrap();
+    Ok(true)
+}
+
+
 
 #[pymodule]
 fn filters(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(grayscale, m)?)?;
     m.add_function(wrap_pyfunction!(blur, m)?)?;
+    m.add_function(wrap_pyfunction!(brighten, m)?)?;
     Ok(())
 }
